f62e562b309a6df5dae8b55b81ad8bd4a776abc5
ichigoat <159642206+stayfatal@users.noreply.github.com>
2024-09-21 09:45:01 +0000

    сделал скрины вместо текста

--- Lab_2/test.asm
+++ Lab_2/test.asm
@@ -2,65 +2,78 @@ format ELF64
 
 public _start
 public exit
-public print_symb
+public display_char
 
 section '.bss' writable
-  num dq 2980480801   
+  num dq 2980480801    
   sum dq 0            
-  ten dq 10                  
+  ten dq 10                 
   buffer db 1         
 
 section '.text' executable
   _start:
     mov rax, [num]      
     xor rbx, rbx            
+
     .sum_loop:
       xor rdx, rdx           
       div qword [ten]         
       add rbx, rdx            
-      test rax, rax              
-      jnz .sum_loop        
-    mov [sum], rbx        
-    call print_symb         
-    mov eax, 60              
-    xor edi, edi        
-    syscall                  
+      cmp rax, 0              
+      jne .sum_loop           
 
-print_symb:
+    mov [sum], rbx       
+
+    call display_char        
+
+    mov eax, 60             
+    xor edi, edi        
+    call exit                  
+
+display_char:
     mov rax, [sum]       
-    mov rdi, 10             
-    xor rbx, rbx             
-    cmp rax, 0
-    je .print_zero
+    xor rbx, rbx            
+
+    cmp rax, 9
+    jle .single_digit       
+
+    mov rcx, 10             
     .loop:
         xor rdx, rdx           
-        div rdi               
+        div rcx                  
         push rdx                
         inc rbx                
         test rax, rax           
-        jnz .loop                 
+        jnz .loop                
+
     .print_loop:
-        pop rax                  
+        pop rax                 
         add rax, '0'             
         mov [buffer], al         
-        mov eax, 1              
-        mov edi, 1              
-        mov rsi, buffer         
-        mov edx, 1              
+
+        mov eax, 1               
+        mov edi, 1               
+        mov rsi, buffer           
+        mov edx, 1             
         syscall
+
         dec rbx                 
-        jnz .print_loop       
-    ret
-    .print_zero:
-        mov byte [buffer], '0'
+        jnz .print_loop      
+
+        ret
+
+    .single_digit:
+        add rax, '0'            
+        mov [buffer], al        
+
         mov eax, 1              
-        mov edi, 1              
-        mov rsi, buffer         
+        mov edi, 1             
+        mov rsi, buffer        
         mov edx, 1              
         syscall
         ret
 
 exit:
-  mov eax, 60
-  xor edi, edi
-  syscall
+  mov eax, 1
+  mov ebx, 0
+  int 0x80
